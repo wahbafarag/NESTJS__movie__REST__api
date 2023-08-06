beb497dee883c5f83fd93f1eb3a1eee02b300d70
Wahba Farag <wahba.farag01@gmail.com>
2023-08-06 19:51:54 +0300

    some work around users

---
+++ src/users/decorators/current-user.decorator.ts
@@ -0,0 +1,13 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { User } from '../schemas/user.schema';
+
+type userType = typeof User;
+
+const CurrentUser = createParamDecorator(
+  (data: userType, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.user;
+
+    // return data ? user && user.data : user;
+  },
+);
---
+++ src/users/dtos/update-user.dto.ts
@@ -0,0 +1,38 @@
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Max,
+  MaxLength,
+  Min,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(4)
+  @MaxLength(20)
+  @IsOptional()
+  name: string;
+
+  @IsNumber()
+  @IsOptional()
+  @IsNotEmpty()
+  @Min(16, { message: 'You are too young to use this app' })
+  @Max(65, { message: 'You are too old to use this app' })
+  age: number;
+
+  @IsString()
+  @IsOptional()
+  @IsNotEmpty({ message: 'Email is required' })
+  email: string;
+
+  @IsString()
+  @IsOptional()
+  @IsNotEmpty()
+  @MinLength(8, { message: 'Password is too short' })
+  @MaxLength(30, { message: 'Password is too long' })
+  password: string;
+}
